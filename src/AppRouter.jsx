ce53ed38d0d0b78774227b2c5291534137a5c183
Yennyferjp <yennyfer.jarava92@gmail.com>
2023-11-10 16:52:41 -0500

    Add InputSection component

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Carousel from './Components/Carousel/Carousel';
+import InputsSection from './Components/InputsSection/InputsSection';
 
 const AppRouter = () => {
   return (
@@ -9,6 +10,8 @@ const AppRouter = () => {
       <Header />
       <Home />
       <Carousel />
+      <InputsSection/>
+
     </div>
   );
 };
