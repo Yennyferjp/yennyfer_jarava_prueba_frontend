2a727b06374778c061132671dffec40a1f167bca
Yennyferjp <yennyfer.jarava92@gmail.com>
2023-11-11 01:30:51 -0500

    Add Footer Componet

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -3,16 +3,17 @@ import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Carousel from './Components/Carousel/Carousel';
 import InputsSection from './Components/InputsSection/InputsSection';
+import Footer from './Components/Footer/Footer';
 
 const AppRouter = () => {
   return (
-    <div>
+    <>
       <Header />
       <Home />
       <Carousel />
       <InputsSection/>
-
-    </div>
+      <Footer/>
+    </>
   );
 };
 
